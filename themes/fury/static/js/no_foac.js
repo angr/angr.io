f1a4a906db9f16fee6aa48d28ed505845089c293
lockshaw <mastakata3@yahoo.com>
2018-01-26 18:12:51 -0800

    Update home page to use new code formatting

--- themes/fury/static/js/no_foac.js
+++ themes/fury/static/js/no_foac.js
@@ -1,11 +1,3 @@
-<style type="text/css">
-  .hidden {display:none;}
-</style>
-<script type="text/javascript" src="/scripts/jquery.js"></script>
-<script type="text/javascript">
-  $('html').addClass('hidden');
-  $(document).ready(function() {
-    $('html').show();
-   });
-</script>
-
+$(document).ready(function() {
+  $('html').show();
+});
